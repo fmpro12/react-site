c6af39ddc1c01a236dc19a4a70c25e351e954b34
Yan Khusid <yankhusid@pingidentity.com>
2023-07-24 16:09:17 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,47 +1,39 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import styled from 'styled-components';
-import Home from './components/Home';
+import './styles.css'; // Import the styles // Add Header import
+import NeuroLines from './components/NeuroLines'; // Add NeuroLines import
+
 import PostList from './components/PostList';
 import Gallery from './components/Gallery';
 
-const Container = styled.div`
-  position: relative;
-  z-index: 1; /* Ensure the container is above the AnimatedBackground */
-`;
+const Container = ({ children }) => {
+  return <div className="container">{children}</div>;
+};
 
-const AnimatedBackground = styled.div`
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background: linear-gradient(45deg, #ee7752, #e73c7e, #23a6d5, #23d5ab);
-  background-size: 400% 400%;
-  animation: gradientAnimation 15s ease infinite, neurographicAnimation 20s linear infinite;
-  z-index: 0; /* Lower stacking order for the AnimatedBackground */
-`;
-
-const HomeAll = () => {
+const Home = () => {
   return (
-    <>
-      <Home />
+    <Container>
+      <div className="post-list">
+        <div className="post">
+          <h2>Welcome to My neurographic Site!</h2>
+          {/* Add any additional content for the home page */}
+        </div>
+      </div>
       <PostList />
       <Gallery />
       {/* Add more sections/content for your one-page site */}
-    </>
+    </Container>
   );
 };
 
 function App() {
   return (
     <Router>
-      <AnimatedBackground /> {/* Place the AnimatedBackground component at the root level */}
-      <Container>
-        <Routes>
-          <Route path="/" element={<HomeAll />} />
-        </Routes>
-      </Container>
+      <div className="animated-background"></div>
+      <NeuroLines /> {/* Add the NeuroLines component */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </Router>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import '../styles.css'; // Import the external CSS file
 
 const Home = () => {
   return (
     <div className="post-list home">
       <div className="post home">
-        <h2>Welcome to My neurographic Site!</h2>
+        <div className="centered-content">
+          <h2 className="transparent-h2">Welcome to My neurographic Site!</h2>
+        </div>
         {/* Add any additional content for the home page */}
       </div>
     </div>
